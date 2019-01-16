c6cff3da0f474d207b333d9d47ef2500cc9bc9e1
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2019-01-16 21:12:04 +0300

    01.standards assignment reworked, .gitignore added

---
+++ 01. standards/03. EcmaScript 5.js/src/js/app.js
@@ -0,0 +1,15 @@
+// This program conforms with ES5 standards
+
+'use strict';
+
+var pointsArray = [74989, 74990, 74990, 62000, 58480, 61800];
+
+var pointsArraySorted = pointsArray.sort().reverse();
+
+var winnerPoints = pointsArraySorted[0];
+
+var medallistAverage = pointsArraySorted.slice(0, 3).reduce((a, b) => a + b) / 3;
+medallistAverage = medallistAverage.toFixed(2);
+
+console.log('Наивысший балл: ' + winnerPoints);
+console.log('Средний балл призёров: ' + medallistAverage);
