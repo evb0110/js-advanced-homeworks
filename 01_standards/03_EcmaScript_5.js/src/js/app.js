bf89c19bc9e49682ed16310646b7a13a0dd9c361
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2019-01-19 20:17:34 +0300

    fixed 03

--- 01_standards/03_EcmaScript_5.js/src/js/app.js
+++ 01_standards/03_EcmaScript_5.js/src/js/app.js
@@ -1,15 +1,21 @@
 // This program conforms with ES5 standards
 
-'use strict';
+"use strict";
 
 var pointsArray = [74989, 74990, 74990, 62000, 58480, 61800];
 
-var pointsArraySorted = pointsArray.sort().reverse();
+var pointsArraySorted = pointsArray.sort(function(a, b) {
+  return b - a;
+});
 
 var winnerPoints = pointsArraySorted[0];
 
-var medallistAverage = pointsArraySorted.slice(0, 3).reduce((a, b) => a + b) / 3;
+var medallistAverage =
+  pointsArraySorted.slice(0, 3).reduce(function(a, b) {
+    return a + b;
+  }) / 3;
+
 medallistAverage = medallistAverage.toFixed(2);
 
-console.log('Наивысший балл: ' + winnerPoints);
-console.log('Средний балл призёров: ' + medallistAverage);
+console.log("Наивысший балл: " + winnerPoints);
+console.log("Средний балл призёров: " + medallistAverage);
