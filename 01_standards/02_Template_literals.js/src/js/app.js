682ae874ff1406c4db334ffcccf46a58475fdf8b
evb0110 <7149553@gmail.com>
2019-01-19 20:09:20 +0300

    fixed 02

--- 01_standards/02_Template_literals.js/src/js/app.js
+++ 01_standards/02_Template_literals.js/src/js/app.js
@@ -1,25 +1,29 @@
 const pointsArray = [523, 6000, 5001];
 
-const ending = (number) => {
+const ending = number => {
   let result;
   if (!Number.isInteger(number)) throw `${number} is not an integer number`;
   const lastDigit = number % 10;
   switch (lastDigit) {
-    case 1: 
-      result = '';
+    case 1:
+      result = "";
       break;
     case 2:
     case 3:
     case 4:
-      result = 'а';
+      result = "а";
       break;
     default:
-      result = 'ов';
+      result = "ов";
   }
   return result;
-}
+};
 
-pointsArray.forEach((number) => {
-  const string = `Ваш баланс: ${number} балл${ending(number)}`;
-  console.log(string);
+pointsArray.forEach(number => {
+  try {
+    const string = `Ваш баланс: ${number} балл${ending(number)}`;
+    console.log(string);
+  } catch (err) {
+    console.error(err);
+  }
 });
