0e8afb368a2a6fc4b91abf58885ff6f180994915
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2019-01-19 20:02:53 +0300

    fixed 01

--- 01_standards/01_EcmaScript_6+.js/src/js/app.js
+++ 01_standards/01_EcmaScript_6+.js/src/js/app.js
@@ -1,21 +1,25 @@
 const bonusStarts = 10000;
 const bonusPercent = 5;
-const prices = [200, 550, 4000, 23, 58, 5000, 485, 711];
+const prices = [200, 550, 4000, 23, 58, 5000, 485, 711, "a"];
 
 const calculateBonus = (starts, percent, prices) => {
-
   let totalPrice = 0;
-  
+
   for (let price of prices) {
-    if (typeof price !== "number") throw(`${price} is a ${typeof price}, not a number!`);
+    if (typeof price !== "number")
+      throw `${price} is a ${typeof price}, not a number!`;
     totalPrice += price;
   }
 
-  let bonus = (totalPrice - starts) * percent / 100;
-  return bonus;
+  return (bonus = ((totalPrice - starts) * percent) / 100);
 };
 
-let result = calculateBonus(bonusStarts, bonusPercent, prices);
-result = Math.round(result);
+let bonus;
 
-console.log(`Размер бонуса: ${result}`);
+try {
+  bonus = calculateBonus(bonusStarts, bonusPercent, prices);
+  bonus = Math.round(bonus);
+  console.log(`Размер бонуса: ${bonus}`);
+} catch (err) {
+  console.error(err);
+}
